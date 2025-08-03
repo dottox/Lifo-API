bfca05c82360777b7963cd40abbb867c51d717d6
dottox <imsm2424@gmail.com>
2025-08-03 16:13:13 -0300

    feat: swagger config

---
+++ src/main/java/dev/dotto/lifoapi/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package dev.dotto.lifoapi.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    // This enables Swagger authentication with JWT Bearer token
+    // It configures the OpenAPI documentation to include a security scheme for JWT Bearer authentication
+    @Bean
+    public OpenAPI customOpenAPI() {
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .description("JWT Bearer Authentication");
+
+        SecurityRequirement bearerRequirement = new SecurityRequirement()
+                .addList("Bearer Authentication");
+
+        Info info = new Info()
+                .title("Lifo API")
+                .version("1.0.0")
+                .description("API documentation for Lifo")
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url("http://www.apache.org/licenses/LICENSE-2.0"))
+                .contact(new Contact()
+                        .name("Ignacio Suárez")
+                        .email("imsm2424@gmail.com"));
+
+        return new OpenAPI()
+                .info(info)
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", bearerScheme))
+                .addSecurityItem(bearerRequirement);
+    }
+}
