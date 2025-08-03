b447686c329a7bb02e628b3019c89fec3f4ab160
dottox <imsm2424@gmail.com>
2025-08-03 14:07:21 -0300

    first commit

---
+++ src/main/java/dev/dotto/lifoapi/LifoApiApplication.java
@@ -0,0 +1,13 @@
+package dev.dotto.lifoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dotto/lifoapi/LifoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dotto.lifoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifoApiApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
